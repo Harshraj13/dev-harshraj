497d10d5f1e9af839353c66c989a65b02d4d265a
Harshraj13 <sadwelkar.7harshraj13@gmail.com>
2022-11-13 18:54:39 +0530

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,127 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project1.png";
+import projImg2 from "../assets/img/project2.png";
+import projImg3 from "../assets/img/project3.png";
+import projImg4 from "../assets/img/project4.png";
+
+import upcommingProImg from "../assets/img/upcommingPro.png";
+
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "MERN Stack Project",
+      description: "Blogging websited from Scratch",
+      imgUrl: projImg2,
+    },
+    {
+      title: "React Website",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    }
+  ];
+  const programming = [
+    {
+      title: "Data Injection media",
+      description: "Data injection based on Standard INS",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Upcomming",
+      description: "SOON!",
+      imgUrl: upcommingProImg,
+    }
+  ];
+  const upcomming = [
+    {
+      title: "Upcomming",
+      description: "SOON!",
+      imgUrl: upcommingProImg,
+    }
+  ];
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Project management is important because it ensures what is being delivered, is right, and will deliver real value against the business opportunity. Every client has strategic goals and the projects that we do for them advance those goals.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Development</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Programming</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">UpComming</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        {
+                          programming.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <Row>
+                        {
+                          upcomming.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
